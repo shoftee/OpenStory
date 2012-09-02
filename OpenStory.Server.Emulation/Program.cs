6910bb2969ee6403d657d1f020c92767cecce616
shoftee <toshosoft@gmail.com>
2012-09-03 01:09:11 +0300

    Lol added TODOs for myself. Silly, isn't it?

--- OpenStory.Server.Emulation/Program.cs
+++ OpenStory.Server.Emulation/Program.cs
@@ -6,6 +6,9 @@ namespace OpenStory.Server.Emulation
     {
         private static void Main()
         {
+            // TODO: Rename project to OpenStory.Server.Management
+            // TODO: Add Management/Diagnostic hooks into all Services. Preferably easily customizable ones. Dunno about dynamic.
+
             Thread.Sleep(Timeout.Infinite);
         }
     }
