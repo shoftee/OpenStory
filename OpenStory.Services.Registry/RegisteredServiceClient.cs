1374d83751db42d0b02b68ebd5199f0d8f8e6943
shoftee <toshosoft@gmail.com>
2012-09-27 00:51:31 +0300

    More minor things...

--- OpenStory.Services.Registry/RegisteredServiceClient.cs
+++ OpenStory.Services.Registry/RegisteredServiceClient.cs
@@ -1,16 +1,12 @@
-﻿using System;
 using System.ServiceModel;
 using System.ServiceModel.Description;
 using OpenStory.Services.Contracts;
 
-namespace OpenStory.Services.Clients
+namespace OpenStory.Services.Registry
 {
     /// <summary>
     /// Represents a client for a generic game service.
     /// </summary>
-    /// <remarks>
-    /// This class is abstract.
-    /// </remarks>
     public sealed class RegisteredServiceClient : DuplexClientBase<IRegisteredService>, IRegisteredService
     {
         /// <summary>
@@ -19,7 +15,7 @@ namespace OpenStory.Services.Clients
         /// <param name="endpoint">The service endpoint information.</param>
         /// <param name="stateChangedHandler">The handler for the service state changes.</param>
         public RegisteredServiceClient(ServiceEndpoint endpoint, IServiceStateChanged stateChangedHandler)
-            : base(new InstanceContext(stateChangedHandler), endpoint)
+            : base(stateChangedHandler, endpoint)
         {
         }
 
