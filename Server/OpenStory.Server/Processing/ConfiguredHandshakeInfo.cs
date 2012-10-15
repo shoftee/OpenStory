c44f0ac5b0d4b43ae94a5bdbf8f5ddef4c3ea397
shoftee <toshosoft@gmail.com>
2012-10-15 21:39:18 +0300

    Adding some pro stuff?

--- Server/OpenStory.Server/Processing/ConfiguredHandshakeInfo.cs
+++ Server/OpenStory.Server/Processing/ConfiguredHandshakeInfo.cs
@@ -1,4 +1,5 @@
 using OpenStory.Common.IO;
+using OpenStory.Server.Fluent;
 
 namespace OpenStory.Server.Processing
 {
@@ -21,6 +22,24 @@ namespace OpenStory.Server.Processing
             this.ClientIv = clientIv;
             this.ServerIv = serverIv;
             this.ServerId = serverId;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="HandshakeInfo"/> with the server-configured values.
+        /// </summary>
+        /// <param name="clientIv">The client cryptographic IV.</param>
+        /// <param name="serverIv">The server cryptographic IV.</param>
+        public ConfiguredHandshakeInfo(byte[] clientIv, byte[] serverIv)
+        {
+            var facade = OS.Config().Domain("Handshake");
+
+            this.Header = facade.Get<ushort>("Header");
+            this.Version = facade.Get<ushort>("Version");
+            this.SubVersion = facade.Get<string>("SubVersion");
+            this.ServerId = facade.Get<byte>("ServerId");
+
+            this.ClientIv = clientIv;
+            this.ServerIv = serverIv;
         }
     }
 }
