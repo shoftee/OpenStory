06c1e9283f64a5cacee8a84fabbda605ffaa6ff1
shoftee <toshosoft@gmail.com>
2013-02-08 21:04:18 +0200

    Doing something weird.

---
+++ Server/OpenStory.Server.Channel/Data/BuddyListEntry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenStory.Server.Channel.Data
+{
+    /// <summary>
+    /// Represents an entry in a player's buddy list.
+    /// </summary>
+    public class BuddyListEntry : ICharacterExtension
+    {
+        /// <summary>
+        /// Gets the identifier of the buddy.
+        /// </summary>
+        public CharacterKey Key { get; private set; }
+
+        /// <summary>
+        /// Gets the group name of the buddy.
+        /// </summary>
+        public string Group { get; private set; }
+
+        /// <summary>
+        /// Gets the channel identifier for the buddy.
+        /// </summary>
+        public int? ChannelId { get; private set; }
+
+        /// <summary>
+        /// Gets whether the buddy is currently visible.
+        /// </summary>
+        public bool Visible { get; private set; }
+
+        /// <summary>
+        /// Gets the status of the buddy.
+        /// </summary>
+        public BuddyListEntryStatus Status { get; private set; }
+    }
+}
---
+++ Server/OpenStory.Server.Channel/Data/BuddyListEntryStatus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OpenStory.Server.Channel.Data
+{
+    /// <summary>
+    /// Denotes the status of the friend relationship.
+    /// </summary>
+    [Serializable]
+    public enum BuddyListEntryStatus
+    {
+        /// <summary>
+        /// The buddy request has not been accepted by the other party.
+        /// </summary>
+        Pending,
+        /// <summary>
+        /// The entry is active and all is good.
+        /// </summary>
+        Active,
+        /// <summary>
+        /// The entry is inactive for whatever reason, e.g. the other party has removed their entry.
+        /// </summary>
+        Inactive,
+    }
+}
