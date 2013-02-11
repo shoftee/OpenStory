4078a3bceca9cd9dc04677f0eb5e501d6a39d12e
shoftee <toshosoft@gmail.com>
2013-02-11 19:54:24 +0200

    Added IntegrationTests project.
    * Also cleaned up Tests solution. A bit.

---
+++ Tests/OpenStory.IntegrationTests/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using OpenStory.Server;
+using OpenStory.Server.Fluent;
+using OpenStory.Server.Modules.Logging;
+using OpenStory.Services.Account;
+using OpenStory.Services.Auth;
+using OpenStory.Services.Contracts;
+using OpenStory.Services.Management;
+using OpenStory.Services.Registry;
+
+namespace OpenStory.IntegrationTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OS.Initialize().Logger(new ConsoleLogger());
+
+            var nexusUri = new Uri("net.tcp://localhost/OpenStory/Registry");
+
+            var registry = new RegistryService();
+            var host = new ServiceHost(registry, nexusUri);
+            host.Open();
+
+            IRegistryService registryClient = new RegistryServiceClient(nexusUri);
+
+            var accountUri = new Uri("net.tcp://localhost/OpenStory/Account");
+            var accountConfig = ServiceConfiguration.Auth(accountUri);
+            var accountGuid = registryClient.RegisterService(accountConfig).Result;
+            var accountInfo = new NexusConnectionInfo(accountGuid, nexusUri);
+
+            string error;
+            Bootstrap.Service(() => new AccountService(), accountInfo, out error);
+            if (error != null)
+            {
+                Console.WriteLine(error);
+                return;
+            }
+
+            var authUri = new Uri("net.tcp://localhost/OpenStory/Auth");
+            var authConfig = ServiceConfiguration.Auth(authUri);
+            var authGuid = registryClient.RegisterService(authConfig).Result;
+            var authInfo = new NexusConnectionInfo(authGuid, nexusUri);
+            Bootstrap.Service(() => new AuthService(), authInfo, out error);
+            if (error != null)
+            {
+                Console.WriteLine(error);
+                return;
+            }
+
+            Console.WriteLine("Woot!");
+        }
+    }
+}
