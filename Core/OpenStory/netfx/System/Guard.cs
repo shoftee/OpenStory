f9e9b3fc3e6c8573f2b8a00311cc3134ba04c1eb
shoftee <shoftee@gmail.com>
2014-02-03 22:54:22 +0200

    Moving Guard class to global namespace.

--- Core/OpenStory/netfx/System/Guard.cs
+++ Core/OpenStory/netfx/System/Guard.cs
@@ -34,71 +34,68 @@ using System;
 using System.Diagnostics;
 using System.Linq.Expressions;
 
-namespace System
+/// <summary>
+/// Common guard class for argument validation.
+/// </summary>
+///	<nuget id="netfx-Guard" />
+[DebuggerStepThrough]
+static class Guard
 {
     /// <summary>
-    /// Common guard class for argument validation.
+    /// Ensures the given <paramref name="value"/> is not null.
+    /// Throws <see cref="ArgumentNullException"/> otherwise.
     /// </summary>
-    ///	<nuget id="netfx-Guard" />
-    [DebuggerStepThrough]
-    static class Guard
+    /// <exception cref="ArgumentException">The <paramref name="value"/> is null.</exception>
+    public static void NotNull<T>(Expression<Func<T>> reference, T value)
     {
-        /// <summary>
-        /// Ensures the given <paramref name="value"/> is not null.
-        /// Throws <see cref="ArgumentNullException"/> otherwise.
-        /// </summary>
-        /// <exception cref="ArgumentException">The <paramref name="value"/> is null.</exception>
-        public static void NotNull<T>(Expression<Func<T>> reference, T value)
-        {
-            if (value == null)
-                throw new ArgumentNullException(GetParameterName(reference), "Parameter cannot be null.");
-        }
+        if (value == null)
+            throw new ArgumentNullException(GetParameterName(reference), "Parameter cannot be null.");
+    }
 
-        /// <summary>
-        /// Ensures the given string <paramref name="value"/> is not null or empty.
-        /// Throws <see cref="ArgumentNullException"/> in the first case, or 
-        /// <see cref="ArgumentException"/> in the latter.
-        /// </summary>
-        /// <exception cref="ArgumentException">The <paramref name="value"/> is null or an empty string.</exception>
-        public static void NotNullOrEmpty(Expression<Func<string>> reference, string value)
-        {
-            NotNull<string>(reference, value);
-            if (value.Length == 0)
-                throw new ArgumentException("Parameter cannot be empty.", GetParameterName(reference));
-        }
+    /// <summary>
+    /// Ensures the given string <paramref name="value"/> is not null or empty.
+    /// Throws <see cref="ArgumentNullException"/> in the first case, or 
+    /// <see cref="ArgumentException"/> in the latter.
+    /// </summary>
+    /// <exception cref="ArgumentException">The <paramref name="value"/> is null or an empty string.</exception>
+    public static void NotNullOrEmpty(Expression<Func<string>> reference, string value)
+    {
+        NotNull<string>(reference, value);
+        if (value.Length == 0)
+            throw new ArgumentException("Parameter cannot be empty.", GetParameterName(reference));
+    }
 
-        /// <summary>
-        /// Ensures the given string <paramref name="value"/> is valid according 
-        /// to the <paramref name="validate"/> function. Throws <see cref="ArgumentNullException"/> 
-        /// otherwise.
-        /// </summary>
-        /// <exception cref="ArgumentException">The <paramref name="value"/> is not valid according 
-        /// to the <paramref name="validate"/> function.</exception>
-        public static void IsValid<T>(Expression<Func<T>> reference, T value, Func<T, bool> validate, string message)
-        {
-            if (!validate(value))
-                throw new ArgumentException(message, GetParameterName(reference));
-        }
+    /// <summary>
+    /// Ensures the given string <paramref name="value"/> is valid according 
+    /// to the <paramref name="validate"/> function. Throws <see cref="ArgumentNullException"/> 
+    /// otherwise.
+    /// </summary>
+    /// <exception cref="ArgumentException">The <paramref name="value"/> is not valid according 
+    /// to the <paramref name="validate"/> function.</exception>
+    public static void IsValid<T>(Expression<Func<T>> reference, T value, Func<T, bool> validate, string message)
+    {
+        if (!validate(value))
+            throw new ArgumentException(message, GetParameterName(reference));
+    }
 
-        /// <summary>
-        /// Ensures the given string <paramref name="value"/> is valid according 
-        /// to the <paramref name="validate"/> function. Throws <see cref="ArgumentNullException"/> 
-        /// otherwise.
-        /// </summary>
-        /// <exception cref="ArgumentException">The <paramref name="value"/> is not valid according 
-        /// to the <paramref name="validate"/> function.</exception>
-        public static void IsValid<T>(Expression<Func<T>> reference, T value, Func<T, bool> validate, string format, params object[] args)
-        {
-            if (!validate(value))
-                throw new ArgumentException(string.Format(format, args), GetParameterName(reference));
-        }
+    /// <summary>
+    /// Ensures the given string <paramref name="value"/> is valid according 
+    /// to the <paramref name="validate"/> function. Throws <see cref="ArgumentNullException"/> 
+    /// otherwise.
+    /// </summary>
+    /// <exception cref="ArgumentException">The <paramref name="value"/> is not valid according 
+    /// to the <paramref name="validate"/> function.</exception>
+    public static void IsValid<T>(Expression<Func<T>> reference, T value, Func<T, bool> validate, string format, params object[] args)
+    {
+        if (!validate(value))
+            throw new ArgumentException(string.Format(format, args), GetParameterName(reference));
+    }
 
-        private static string GetParameterName(Expression reference)
-        {
-            var lambda = reference as LambdaExpression;
-            var member = lambda.Body as MemberExpression;
+    private static string GetParameterName(Expression reference)
+    {
+        var lambda = reference as LambdaExpression;
+        var member = lambda.Body as MemberExpression;
 
-            return member.Member.Name;
-        }
+        return member.Member.Name;
     }
 }
