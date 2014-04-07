08062e64bea33490d1ce28ed492dcfcb7553944e
shoftee <shoftee@gmail.com>
2014-04-08 00:21:27 +0300

    Adding some inane crypto.

---
+++ Core/OpenStory/Cryptography/HeartbeatCrypto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenStory.Cryptography
+{
+    /// <summary>
+    /// Some kinda magic used to please the HackShield gods.
+    /// </summary>
+    public sealed class HeartbeatCrypto
+    {
+        /// <summary>
+        /// Transforms the provided heartbeat request.
+        /// </summary>
+        public static int TransformClientRequest(int request)
+        {
+            int right = request & (~0x7F);
+            int middleRight = request & (~0x1F);
+            int middleLeft = (request & (0x18)) ^ 0x10;
+            int left = 7 - (request & 7);
+
+            int response = (right | middleRight | middleLeft | left) - 1;
+            return response;
+        }
+    }
+}
